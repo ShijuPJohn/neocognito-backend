73857ea4a94d139741704bea8c1e19cb0e339478
ShijuPJohn <shijupjohn4u@gmail.com>
2024-04-08 04:58:17 +0530

    total attempts and marks obtained fields added to questionset model

--- models/questionModels.go
+++ models/questionModels.go
@@ -24,19 +24,20 @@ type Question struct {
 }
 
 type QuestionSet struct {
-	ID             string    `json:"id" bson:"_id,omitempty"`
-	Name           string    `json:"name" bson:"name"`
-	Questions      []string  `json:"questions" bson:"questions" validate:""`
-	Category       string    `json:"category,omitempty" bson:"category" validate:"required"`
-	Subject        string    `json:"subject,omitempty" bson:"subject" validate:"required"`
-	Tags           []string  `json:"tags,omitempty" bson:"tags" validate:"required"`
-	Exam           string    `json:"exam,omitempty" bson:"exam"`
-	Language       string    `json:"language,omitempty" bson:"language" validate:"required"`
-	Difficulty     int       `json:"difficulty,omitempty" bson:"difficulty"`
-	CorrectOptions []int     `json:"correctOptions,omitempty" bson:"correctOptions" validate:"required"`
-	Explanation    string    `json:"explanation,omitempty" bson:"explanation"`
-	CreatedAt      time.Time `json:"createdAt,omitempty"`
-	EditedAt       time.Time `json:"editedAt,omitempty"`
-	CreatedById    string    `json:"createdBy,omitempty" bson:"createdBy,omitempty"`
-	EditedByIds    []string  `json:"editedBy,omitempty" bson:"editedBy,omitempty"`
+	ID            string    `json:"id" bson:"_id,omitempty"`
+	Name          string    `json:"name" bson:"name"`
+	Questions     []string  `json:"questions" bson:"questions" validate:""`
+	Category      string    `json:"category,omitempty" bson:"category" validate:"required"`
+	Subject       string    `json:"subject,omitempty" bson:"subject" validate:"required"`
+	Tags          []string  `json:"tags,omitempty" bson:"tags" validate:"required"`
+	Exam          string    `json:"exam,omitempty" bson:"exam"`
+	Language      string    `json:"language,omitempty" bson:"language" validate:"required"`
+	Difficulty    int       `json:"difficulty,omitempty" bson:"difficulty"`
+	Explanation   string    `json:"explanation,omitempty" bson:"explanation"`
+	CreatedAt     time.Time `json:"createdAt,omitempty"`
+	EditedAt      time.Time `json:"editedAt,omitempty"`
+	CreatedById   string    `json:"createdBy,omitempty" bson:"createdBy,omitempty"`
+	EditedByIds   []string  `json:"editedBy,omitempty" bson:"editedBy,omitempty"`
+	TotalAttempts int       `json:"totalAttempts,omitempty" bson:"totalAttempts"`
+	MarksObtained []int     `json:"marksObtained,omitempty" bson:"marksObtained"`
 }
