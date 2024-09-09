b45f36064ced1a7d8b130ba01864608dbfef6314
ShijuPJohn <shijupjohn4u@gmail.com>
2024-09-09 22:16:45 +0530

    feat: create questionset and also get questionsets.

--- controllers/questionSetControllers.go
+++ controllers/questionSetControllers.go
@@ -1,82 +1,143 @@
 package controllers
 
-//
-//import (
-//	"github.com/go-playground/validator/v10"
-//	"github.com/gofiber/fiber/v2"
-//	"github.com/golang-jwt/jwt/v4"
-//	"go.mongodb.org/mongo-driver/bson"
-//	"go.mongodb.org/mongo-driver/bson/primitive"
-//	"go.mongodb.org/mongo-driver/mongo"
-//	"go.mongodb.org/mongo-driver/mongo/options"
-//	"neocognito-backend/models"
-//	"neocognito-backend/utils"
-//	"strconv"
-//	"strings"
-//)
-//
-//func CreateQuestionSet(c *fiber.Ctx) error {
-//	user := c.Locals("user").(*jwt.Token)
-//	claims := user.Claims.(jwt.MapClaims)
-//	q := new(models.QuestionSet)
-//	if err := c.BodyParser(&q); err != nil {
-//		return c.Status(400).SendString(err.Error())
-//	}
-//	validate := validator.New()
-//	err := validate.Struct(q)
-//	if err != nil {
-//		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-//			"status":  "error",
-//			"message": err.Error(),
-//		})
-//	}
-//	q.CreatedById = claims["id"].(string)
-//	insertionResult, err := utils.Mg.Db.Collection("question_set").InsertOne(c.Context(), q)
-//	if err != nil {
-//		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-//			"status":  "error",
-//			"message": err.Error(),
-//		})
-//	}
-//	return c.Status(fiber.StatusOK).JSON(fiber.Map{"status": "success", "question_set": q, "id": insertionResult.InsertedID})
-//}
-//func GetQuestionSets(c *fiber.Ctx) error {
-//	findOptions := options.Find()
-//	filter := bson.D{}
-//
-//	// Parse query parameters
-//	if subject := c.Query("subject"); subject != "" {
-//		filter = append(filter, bson.E{Key: "subject", Value: subject})
-//	}
-//	if category := c.Query("category"); category != "" {
-//		filter = append(filter, bson.E{Key: "category", Value: category})
-//	}
-//	if tags := c.Query("tags"); tags != "" {
-//		tagsList := strings.Split(tags, ",")
-//		filter = append(filter, bson.E{Key: "tags", Value: bson.M{"$in": tagsList}})
-//	}
-//	if language := c.Query("language"); language != "" {
-//		filter = append(filter, bson.E{Key: "language", Value: language})
-//	}
-//
-//	// Count of sets
-//	count, _ := strconv.Atoi(c.Query("count", "-1"))
-//	if count != -1 {
-//		findOptions.SetLimit(int64(count))
-//	}
-//
-//	cursor, err := utils.Mg.Db.Collection("question_set").Find(c.Context(), filter, findOptions)
-//	if err != nil {
-//		return handleError(c, fiber.StatusInternalServerError, err.Error())
-//	}
-//
-//	var questionSets []models.QuestionSet
-//	if err := cursor.All(c.Context(), &questionSets); err != nil {
-//		return handleError(c, fiber.StatusInternalServerError, err.Error())
-//	}
-//
-//	return c.Status(fiber.StatusOK).JSON(fiber.Map{"question_sets": questionSets})
-//}
+import (
+	"fmt"
+	"github.com/go-playground/validator/v10"
+	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v5"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	"neocognito-backend/models"
+	"neocognito-backend/utils"
+	"reflect"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func CreateQuestionSet(c *fiber.Ctx) error {
+	type QuestionSetDT0Model struct {
+		Questions   []string `json:"questions" validate:"required"`
+		Subject     string   `json:"subject" validate:"required"`
+		Name        string   `json:"name" validate:"required"`
+		Tags        []string `json:"tags" validate:"required"`
+		Description string   `json:"description" validate:"required"`
+		Exam        string   `json:"exam" validate:"required"`
+	}
+	var qSetDTO QuestionSetDT0Model
+	user := c.Locals("user").(*jwt.Token)
+	claims := user.Claims.(jwt.MapClaims)
+	q := new(models.QuestionSet)
+	if err := c.BodyParser(&qSetDTO); err != nil {
+		return c.Status(400).SendString(err.Error())
+	}
+	validate := validator.New()
+	err := validate.Struct(qSetDTO)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"status":  "error",
+			"message": err.Error(),
+		})
+	}
+	q.Name = qSetDTO.Name
+	q.Description = qSetDTO.Description
+	userID := claims["id"].(string)
+	q.CreatedById = userID
+	q.EditedByIds = []string{userID}
+	currentTime := time.Now()
+	q.CreatedAt = currentTime
+	q.EditedAt = currentTime
+	q.Subject = qSetDTO.Subject
+	q.Tags = qSetDTO.Tags
+	q.Exam = qSetDTO.Exam
+	var objectIDs []primitive.ObjectID
+	for _, idStr := range qSetDTO.Questions {
+		id, err := primitive.ObjectIDFromHex(idStr)
+		if err != nil {
+			continue
+		}
+		objectIDs = append(objectIDs, id)
+	}
+	projection := bson.M{"id": 1, "correct_options": 1}
+	findOptions := options.Find()
+	findOptions.SetProjection(projection)
+
+	filter := bson.M{"_id": bson.M{"$in": objectIDs}}
+	fmt.Println("before querying")
+	cursor, err := utils.Mg.Db.Collection("questions").Find(c.Context(), filter, findOptions)
+	fmt.Println("after querying")
+	var results []bson.M
+	if err := cursor.All(c.Context(), &results); err != nil {
+		log.Fatal(err)
+	}
+	qIDs := make([]string, 0)
+	correctOptions := make([][]int, 0)
+	for _, result := range results {
+		qIDString := result["_id"].(primitive.ObjectID).Hex()
+		qIDs = append(qIDs, qIDString)
+		//correctOptions = append(correctOptions, result["correct_options"].(string))
+		fmt.Println(result["correct_options"])
+		fmt.Println(reflect.TypeOf(result["correct_options"]))
+		var intSlice []int
+		for _, i := range result["correct_options"].(primitive.A) {
+			intSlice = append(intSlice, int(i.(int32)))
+		}
+		correctOptions = append(correctOptions, intSlice)
+	}
+	q.QIDList = qIDs
+	q.CorrectAnswerList = correctOptions
+
+	insertionResult, err := utils.Mg.Db.Collection("question_set").InsertOne(c.Context(), q)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"status":  "error",
+			"message": err.Error(),
+		})
+	}
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{"status": "success", "question_set": q, "id": insertionResult.InsertedID})
+}
+
+func GetQuestionSets(c *fiber.Ctx) error {
+	findOptions := options.Find()
+	filter := bson.D{}
+
+	if subject := c.Query("subject"); subject != "" {
+		filter = append(filter, bson.E{Key: "subject", Value: subject})
+	}
+	if category := c.Query("category"); category != "" {
+		filter = append(filter, bson.E{Key: "category", Value: category})
+	}
+	if exam := c.Query("exam"); exam != "" {
+		filter = append(filter, bson.E{Key: "exam", Value: exam})
+	}
+	if name := c.Query("name"); name != "" {
+		filter = append(filter, bson.E{Key: "name", Value: name})
+	}
+	if tags := c.Query("tags"); tags != "" {
+		tagsList := strings.Split(tags, ",")
+		filter = append(filter, bson.E{Key: "tags", Value: bson.M{"$in": tagsList}})
+	}
+
+	count, _ := strconv.Atoi(c.Query("count", "-1"))
+	if count != -1 {
+		findOptions.SetLimit(int64(count))
+	}
+
+	cursor, err := utils.Mg.Db.Collection("question_set").Find(c.Context(), filter, findOptions)
+	if err != nil {
+		return handleError(c, fiber.StatusInternalServerError, err.Error())
+	}
+
+	var questionSets []models.QuestionSet
+	if err := cursor.All(c.Context(), &questionSets); err != nil {
+		return handleError(c, fiber.StatusInternalServerError, err.Error())
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{"question_sets": questionSets})
+}
+
 //func EditQuestionSet(c *fiber.Ctx) error {
 //	idParam := c.Params("id")
 //	qID, err := primitive.ObjectIDFromHex(idParam)
